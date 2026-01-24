dc42f1dfa9f2f4a5936dc56101ffb0a66f508dd3
nevermore52 <kolakalinin9000@gmail.com>
2026-01-24 14:35:07 +0300

    v.1.2.8(instruct)

--- internal/services/user_service.go
+++ internal/services/user_service.go
@@ -135,7 +135,7 @@ func (s *UserService) CreateUser(telegramID int64, username, firstName, lastName
 	}
 
 	if _, quotaErr := s.db.Exec(`
-		INSERT INTO user_quotas (telegram_id, text_daily, text_extra, image_monthly, image_extra, music_monthly, music_extra, video_monthly, video_extra)
+		INSERT INTO user_quotas (telegram_id, text_daily, text_extra, image_weekly, image_extra, music_weekly, music_extra, video_weekly, video_extra)
 		VALUES ($1, 10, 0, 0, 0, 0, 0, 0, 0)
 		ON CONFLICT (telegram_id) DO NOTHING
 	`, telegramID); quotaErr != nil {
