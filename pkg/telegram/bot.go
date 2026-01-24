1f640be6c3b7d0a28bdee1b725120501c0992858
nevermore52 <kolakalinin9000@gmail.com>
2026-01-24 12:08:02 +0300

    v.1.2.4(English added)

--- pkg/telegram/bot.go
+++ pkg/telegram/bot.go
@@ -166,7 +166,7 @@ func (b *Bot) Config() *config.Config {
 
 func (b *Bot) sendBuyExtrasCategory(chatID int64, userID int64, category string, callbackPrefix string) {
 	loc := b.getLocalization(userID)
-	packs := []int{10, 20, 50, 100, 200, 500}
+	packs := []int{10, 50, 100, 250, 500}
 
 	if category == "" {
 		parts := strings.Split(callbackPrefix, ":")
