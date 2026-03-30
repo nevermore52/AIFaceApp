5e8e61684d2070f958d8fe4ae379f7efc6ea6c04
nevermore52 <kolakalinin9000@gmail.com>
2026-03-30 23:37:18 +0300

    v2.4.7

--- web/migrations/000005_add_user_fk_to_generations.up.sql
+++ web/migrations/000005_add_user_fk_to_generations.up.sql
@@ -2,11 +2,18 @@
 -- First, we need to update any existing generation_requests that have invalid user_ids
 -- This assumes that old generations used telegram_id directly as user_id
 
+-- Delete generation_requests that don't have a corresponding user
+DELETE FROM generation_requests
+WHERE user_id NOT IN (SELECT id FROM users)
+  AND user_id NOT IN (SELECT telegram_id FROM users WHERE telegram_id IS NOT NULL);
+
 -- Update generation_requests to use users.id instead of telegram_id
 UPDATE generation_requests gr
 SET user_id = u.id
 FROM users u
-WHERE gr.user_id = u.telegram_id AND gr.user_id != u.id;
+WHERE u.telegram_id IS NOT NULL 
+  AND gr.user_id = u.telegram_id 
+  AND gr.user_id != u.id;
 
 -- Now add the foreign key constraint
 ALTER TABLE generation_requests
